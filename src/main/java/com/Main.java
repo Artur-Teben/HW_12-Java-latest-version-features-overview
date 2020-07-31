1339b3a424e9ee063cbc7231bb6722e406a48c0e
Artur Teben <teben.artur99@gmail.com>
2020-07-31 15:16:40 +0300

    Call method runMenu

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,11 @@
+package main.java.com;
+
+import view.Menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.runMenu();
+    }
+}
